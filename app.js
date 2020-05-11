cb7217d8136daecb7ff5c4498749e24849185ec3
gabrielacarr <briecardo@icloud.com>
2020-05-11 16:05:18 -0400

    sendFile

--- app.js
+++ app.js
@@ -5,8 +5,26 @@ const port = 3000 // not neccessay, but easier to change if in multiple places
 
 
 app.get('/', (req, res) => {
-    res.send("hello")
-    // res.sendFile(__dirname + '/views/home.html')
+    //res.send("hello")
+    res.sendFile(__dirname + '/views/home.html') // to send file 
 } )
+// express global variable _dirname to retreive entire path
+// equivalent to using pwd on terminal to retreive entire path
+// _dirname is exact path + the remaining portion of path
 
-app.listen(port) // to run the express app
+// get and send are methods of express
+// take agruments
+// have two params
+// first param : the path the cilent requests, the end point
+// second param : the request
+
+// req and res are objects of information
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+}) 
+// to run the express app
+// listen can take a second param, to callback
+// nodemon.app js in terminal
+// prints: Server running on port 3000
+
